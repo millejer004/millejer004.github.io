c2a7925845a2473ed5ff43ab5cb62769de29e485
millejer004 <65995211+millejer004@users.noreply.github.com>
2021-02-02 21:28:04 -0800

    p

--- mail.php
+++ mail.php
@@ -8,9 +8,9 @@ $priority = $_POST['priority'];
 $type = $_POST['type'];
 $message = $_POST['message'];
 $formcontent=" From: $name \n Phone: $phone \n Call Back: $call \n Website: $website \n Priority: $priority \n Type: $type \n Message: $message";
-$recipient = "YOUREMAIL@HERE.COM";
+$recipient = "millejer004@gmail.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
-mail($name, $message) or die("Error!");
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 echo "Thank You!" . " -" . "<a href='index.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
 ?>
